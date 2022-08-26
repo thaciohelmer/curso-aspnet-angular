164d037db0ccf25c72332d2cb9ec600b7d96bb5e
Thácio Helmer <thaciohelmer@hotmail.com>
2022-08-25 21:58:25 -0300

    Seção 4: Angular

--- BACK/src/ProEventos.API/Startup.cs
+++ BACK/src/ProEventos.API/Startup.cs
@@ -26,6 +26,7 @@ namespace ProEventos.API
                     context.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -49,6 +50,8 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
